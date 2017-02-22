54697f8b8045ca4ade900fdc70d7c5e1c3ff3906
Wei Zhou <867571123@qq.com>
2017-02-22 10:46:42 +0800

    添加js

---
+++ mobile/js/swiperslide.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    slidesPerView: 1,
+    centeredSlides: true,
+    paginationClickable: true,
+    spaceBetween: 0,
+    autoplay: 2500,
+});
